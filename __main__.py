74dfe85cbc9db44cba19d8ba07ce129c41505633
jasnatbyui <jasnatbyui@gmail.com>
2022-03-27 14:36:59 +1300

    Complete def(main) added

--- __main__.py
+++ __main__.py
@@ -30,6 +30,7 @@ pygame.font.init()
 pygame.display.set_caption(config.TITLE)
 pygame.display.set_icon(Image.PLAYER_SPACE_SHIP)
 
+
 def main():
     title_font = pygame.font.Font(Font.edit_undo_font, 82)
 
@@ -43,7 +44,7 @@ def main():
     settings_btn = IconButton(Image.EXIT_IMAGE)
 
     exit_btn = IconButton(Image.EXIT_IMAGE)
-    
+
     run = True
     while run:
         pygame.mouse.set_visible(True)
@@ -60,9 +61,9 @@ def main():
             (config.center_x - 210, config.center_y + 42), (195, 66))
         keyboard_btn.draw(
             (config.center_x + 15, config.center_y + 42), (195, 66))
-        
+
         Assets.text.draw('Start Game', title_font, Colors.WHITE,
-                        (config.center_x, config.center_y-10), True, True)
+                         (config.center_x, config.center_y-10), True, True)
 
         # Control Page
         control_btn.draw((config.starting_x + 65, 53), True, True)
@@ -70,14 +71,110 @@ def main():
         # ScoreBoard Page
         trophy_btn.draw((config.ending_x - 65, 55), True, True)
 
-        #Settings Page
+        # Settings Page
         settings_btn.draw((config.ending_x - 65, 165), True, True)
 
-        #Shis Page
+        # Shis Page
         ships_btn.draw((config.starting_x + 65, 165), True, True)
 
         audio_cfg.display_volume()
 
-        
+        exit_btn.draw((config.ending_x - 75, config.ending_y - 40), True, True)
+
+        Assets.image.draw(Image.TITLE_LOGO, (config.center_x, 50), True)
+
+        pygame.display.flip()
+        config.clock.tick(config.FPS)  # capping frame rate to 60
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if event.type == pygame.VIDEORESIZE:
+                if not display_cfg.fullscreen:
+                    config.update(event.w, event.h)
+
+            # Keyboard events
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_m:
+                    audio_cfg.toggle_mute()
+                if event.key == pygame.K_PLUS or event.key == pygame.K_EQUALS:
+                    audio_cfg.inc_volume(5)
+                if event.key == pygame.K_MINUS:
+                    audio_cfg.dec_volume(5)
+                if event.key == pygame.K_f:
+                    config.update(
+                        config.monitor_size[0], config.monitor_size[1])
+                    display_cfg.toggle_full_screen()
+
+        # Mouse click events
+        if event.type == pygame.MOUSEBOTTOMDOWN:
+            if event.button == 1:
+                if mouse_btn.isOver():
+                    game(True)
+                if keyboard_btn.isOver():
+                    game()
+                if control_btn.isOver():
+                    controls()
+                if trophy_btn.isOver():
+                    score_board()
+                if ships_btn.isOver():
+                    ships()
+                if settings_btn.isOver():
+                    settings()
+                if exit_btn.isOver():
+                    run = False
+
+        # Mouse hover events
+        if event.type == pygame.MOUSEMOTION:
+            if mouse_btn.isOver():
+                mouse_btn.outline = True
+            else:
+                mouse_btn.outline = False
+
+            if keyboard_btn.isOver():
+                keyboard_btn.outline = True
+            else:
+                keyboard_btn.outline = False
+
+            if control_btn.isOver():
+                control_btn.outline = True
+            else:
+                control_btn.outline = False
+
+            if trophy_btn.isOver():
+                trophy_btn.outline = True
+            else:
+                trophy_btn.outline = False
+
+            if settings_btn.isOver():
+                settings_btn.outline = True
+            else:
+                settings_btn.outline = False
+
+            if ships_btn.isOver():
+                ships_btn.outline = True
+            else:
+                ships_btn.outline = False
+
+            if exit_btn.isOver():
+                exit_btn.outline = True
+            else:
+                exit_btn.outline = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_ESCAPE] or keys[pygame.K_q]:
+        run = False
+
+    if keys[pygame.K_c]:
+        controls()
+
+    if keys[pygame.K_s]:
+        score_board()
 
 
+pygame.guit()
+sys.exit(0)
+
+if __name__ == "__main__":
+    main()
